b21c72ecfda31f110d1f45e779463c0a7c2138d0
Utsav Pachouri <pachouriutsav@gmail.com>
2025-07-28 03:27:28 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/team.tsx
+++ src/components/team.tsx
@@ -14,7 +14,7 @@ const teamMembers = [
   {
     name: 'John Smith',
     title: 'Lead Developer',
-    avatarUrl: 'https://placehold.co/128x128.png',
+    avatarUrl: 'https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw3fHxwcm9maWxlJTIwcGhvdG98ZW58MHx8fHwxNzUzNjczMjIyfDA&ixlib=rb-4.1.0&q=80&w=1080',
     socials: {
       linkedin: '#',
       twitter: '#',
