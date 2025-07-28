2172b6f7b280d6f5175db308c3ac6853103fba2f
Utsav Pachouri <pachouriutsav@gmail.com>
2025-07-28 12:44:03 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <a href="tel:+9193

--- src/components/ui/star-border.tsx
+++ src/components/ui/star-border.tsx
@@ -5,15 +5,11 @@ import { cn } from "@/lib/utils";
 const StarBorder = ({
   as: Component = "div",
   className = "",
-  color = "hsl(var(--primary))",
-  speed = "4s",
   children,
   ...rest
 }: {
   as?: React.ElementType;
   className?: string;
-  color?: string;
-  speed?: string;
   children: React.ReactNode;
   [key: string]: any;
 }) => {
@@ -22,20 +18,6 @@ const StarBorder = ({
       className={cn("star-border-container", className)}
       {...rest}
     >
-      <div
-        className="border-gradient-bottom"
-        style={{
-          background: `radial-gradient(circle, ${color}, transparent 40%)`,
-          animationDuration: speed,
-        }}
-      ></div>
-      <div
-        className="border-gradient-top"
-        style={{
-          background: `radial-gradient(circle, ${color}, transparent 40%)`,
-          animationDuration: speed,
-        }}
-      ></div>
       <div className="inner-content">{children}</div>
     </Component>
   );
