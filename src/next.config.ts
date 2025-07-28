b94e55d92089e7af375b2af336ff85b6f0892039
Utsav Pachouri <pachouriutsav@gmail.com>
2025-07-28 03:51:04 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ src/next.config.ts
@@ -0,0 +1,39 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  experimental: {
+    
+  },
+  allowedDevOrigins: ["https://9000-firebase-studio-1753663047046.cluster-73qgvk7hjjadkrjeyexca5ivva.cloudworkstations.dev"],
+};
+
+export default nextConfig;
