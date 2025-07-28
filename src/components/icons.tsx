a63777e5c69e6092443794d23e6047c86ba1f4b5
Utsav Pachouri <pachouriutsav@gmail.com>
2025-07-28 01:37:07 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <text x="0" y="25"

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,23 +1,23 @@
 export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
     <svg
-      width="250"
-      height="32"
-      viewBox="0 0 250 32"
+      width="200"
+      height="28"
+      viewBox="0 0 200 28"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
       <defs>
-        <linearGradient id="logo-gradient" x1="0" y1="16" x2="250" y2="16" gradientUnits="userSpaceOnUse">
+        <linearGradient id="logo-gradient" x1="0" y1="14" x2="200" y2="14" gradientUnits="userSpaceOnUse">
           <stop stopColor="#A3FF12"/>
           <stop offset="1" stopColor="#00FFEE"/>
         </linearGradient>
       </defs>
       <text
         x="0"
-        y="25"
+        y="22"
         fontFamily="Plus Jakarta Sans, sans-serif"
-        fontSize="32"
+        fontSize="26"
         fontWeight="bold"
         fill="url(#logo-gradient)"
       >
