7ac3e2bd7861bb351608a7602873e78efd8d13eb
Utsav Pachouri <pachouriutsav@gmail.com>
2025-07-28 13:44:35 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -33,7 +33,10 @@ const nextConfig: NextConfig = {
   experimental: {
     
   },
-  allowedDevOrigins: ["https://9000-firebase-studio-1753663047046.cluster-73qgvk7hjjadkrjeyexca5ivva.cloudworkstations.dev"],
+  allowedDevOrigins: [
+    "https://9000-firebase-studio-1753663047046.cluster-73qgvk7hjjadkrjeyexca5ivva.cloudworkstations.dev",
+    "https://6000-firebase-studio-1753663047046.cluster-73qgvk7hjjadkrjeyexca5ivva.cloudworkstations.dev"
+  ],
 };
 
 export default nextConfig;
