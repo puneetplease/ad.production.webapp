cd7421acf129052b974b68e292935ae51e4a614f
Utsav Pachouri <pachouriutsav@gmail.com>
2025-07-28 22:40:36 +0000

    import React from 'react'; import { DotLottieReact } from '@lottiefiles/

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -8,14 +8,8 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import Link from 'next/link';
 import { useCart } from '@/hooks/use-cart';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      'dotlottie-wc': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & { src: string; style: React.CSSProperties, speed: string, autoplay: boolean, loop: boolean }, HTMLElement>;
-    }
-  }
-}
 
 export default function ThankYouPage() {
   const { clearCart } = useCart();
@@ -30,14 +24,13 @@ export default function ThankYouPage() {
       <main className="flex-1 flex items-center justify-center py-16 sm:py-24 lg:py-32">
         <div className="container mx-auto px-4 flex justify-center">
           <Card className="w-full max-w-lg text-center overflow-hidden">
-             <CardHeader className="p-8 bg-card relative flex justify-center items-center">
-                <dotlottie-wc 
-                    src="https://lottie.host/87f242ad-a531-44da-9d1b-a8bb7587a3a2/EdzkMw9aMc.lottie" 
-                    style={{width: "300px", height: "300px"}} 
-                    speed="1" 
-                    autoplay 
-                    loop>
-                </dotlottie-wc>
+             <CardHeader className="p-0 bg-card relative flex justify-center items-center">
+                <DotLottieReact
+                    src="https://lottie.host/87f242ad-a531-44da-9d1b-a8bb7587a3a2/EdzkMw9aMc.lottie"
+                    loop
+                    autoplay
+                    style={{width: "300px", height: "300px"}}
+                />
             </CardHeader>
             <CardContent className="p-8 pt-0">
               <CardTitle className="font-headline text-3xl">Thank You for Your Order!</CardTitle>
