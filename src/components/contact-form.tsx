a02c9c5aa8097ff4b37729ce7296b1da0bb03240
Utsav Pachouri <pachouriutsav@gmail.com>
2025-07-28 18:32:33 +0000

    <form action="https://formspree.io/f/{form_id}" method="post"> <label fo

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -34,7 +34,7 @@ export default function ContactForm() {
               </div>
               <div className="space-y-2">
                 <Label htmlFor="email">Email</Label>
-                <Input id="email" name="email" type="email" placeholder="your@email.com" required />
+                <Input id="email" name="Email" type="email" placeholder="your@email.com" required />
               </div>
               <div className="space-y-2">
                 <Label htmlFor="phone">Phone Number</Label>
