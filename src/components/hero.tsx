06b12b584e8c1be792d33e2055a32d4c0f01411c
UTSAV PACHOURI <pachouriustav@gmail.com>
2025-07-30 13:32:45 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button size="lg"

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -49,7 +49,7 @@ export default function Hero() {
                       Watch Our Reel
                     </Button>
                   </DialogTrigger>
-                  <DialogContent className="p-0 border-0 bg-transparent w-auto h-auto max-w-[90vw] max-h-[90vh]">
+                  <DialogContent className="p-0 border-0 bg-transparent w-screen h-screen max-w-none">
                     <div className="aspect-video w-full h-full">
                         <iframe allow="fullscreen;autoplay" allowFullScreen height="100%" src="https://streamable.com/e/xxbi6o?autoplay=1" width="100%" style={{border:"none", width:"100%", height:"100%", overflow:"hidden"}}></iframe>
                     </div>
